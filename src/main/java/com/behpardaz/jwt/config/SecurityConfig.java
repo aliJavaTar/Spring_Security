d500ae0e11633781ef3e6878d0aac6829403b6ef
alijavaboy <ali77erfagh99@gmail.com>
2022-06-27 20:17:55 +0430

    permison for url

--- src/main/java/com/behpardaz/jwt/config/SecurityConfig.java
+++ src/main/java/com/behpardaz/jwt/config/SecurityConfig.java
@@ -1,7 +1,15 @@
 package com.behpardaz.jwt.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeHttpRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated();
+    }
 }
